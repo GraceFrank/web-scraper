3f72120b9ca56e6ddb4ce4192b1678dcd4b624e3
Grace Frank <grace.frank@centricityinsights.com>
2020-07-21 00:47:11 +0100

    got page title from page

--- scrapper.js
+++ scrapper.js
@@ -1,4 +1,24 @@
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 
-const url = 'https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/';
+const urls = [
+    'https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/',
+];
+
+puppeteer
+    .launch()
+    .then(browser => browser.newPage())
+    .then(page => {
+        return page.goto(url).then(function () {
+            return page.content();
+        });
+    })
+    .then(html => {
+        const $ = cheerio.load(html);
+
+        // get page title
+        const pageTitle = $('title').text()
+
+        console.log(pageTitle);
+    })
+    .catch(console.error); 
