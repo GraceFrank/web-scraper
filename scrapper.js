fe8d6bad27fc6a48f9e266fa6a89f04ef1142933
Grace Frank <grace.frank@centricityinsights.com>
2020-07-21 12:15:44 +0100

    got page title

--- scrapper.js
+++ scrapper.js
@@ -3,22 +3,31 @@ const puppeteer = require('puppeteer');
 
 const urls = [
     'https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/',
+    'https://www.reddit.com/r/news/',
+    'https://pusher.com/tutorials/web-scraper-node'
+
 ];
 
-puppeteer
-    .launch()
-    .then(browser => browser.newPage())
-    .then(page => {
-        return page.goto(url).then(function () {
-            return page.content();
-        });
-    })
-    .then(html => {
-        const $ = cheerio.load(html);
+const scraper = url => {
+    puppeteer
+        .launch()
+        .then(browser => browser.newPage())
+        .then(page => {
+            return page.goto(url).then(function () {
+                return page.content();
+            });
+        })
+        .then(html => {
+            const $ = cheerio.load(html);
 
-        // get page title
-        const pageTitle = $('title').text()
+            // get page title from the html head
+            const pageTitle = $('title', 'head').text()
 
-        console.log(pageTitle);
-    })
-    .catch(console.error); 
+            console.log('PageTitle', pageTitle);
+        })
+        .catch(console.error);
+}
+
+for (const url of urls)
+    scraper(url);
+
