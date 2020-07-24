3a18aa83a30fcfde8207da0c6df3adfa4ce3628c
Grace Frank <grace.frank@centricityinsights.com>
2020-07-24 17:27:20 +0100

    refactored scrapper

--- scrapper.js
+++ scrapper.js
@@ -1,46 +1,36 @@
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 
-const urls = [
-    'https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/',
-    // 'https://www.reddit.com/r/news/',
-    // 'https://pusher.com/tutorials/web-scraper-node'
-
-];
-
 const scraper = async url => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+        const html = await page.content()
+        const $ = cheerio.load(html);
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    const html = await page.content()
-    const $ = cheerio.load(html);
+        // get page title from the html head
+        const pageTitle = $('title', 'head').text()
 
-    // get page title from the html head
-    const pageTitle = $('title', 'head').text()
+        //get page Description from metatag
+        const pageDescription = $("meta[name|='description'], meta[property|='og\\:description']")
+            .attr('content')
 
-    //get page Description from metatag
-    const pageDescription = $("meta[name|='description'], meta[property|='og\\:description']")
-        .attr('content')
+        //get largest image on  page
+        const largestImage = await page.evaluate(() => {
+            return [...document.getElementsByTagName('img')].sort((a, b) => b.clientWidth * b.clientHeight - a.clientWidth * a.clientHeight)[0].src;
+        });
 
+        return { pageTitle, pageDescription, largestImage }
 
-    const largestImage = await page.evaluate(() => {
-        return [...document.getElementsByTagName('img')].sort((a, b) => b.clientWidth * b.clientHeight - a.clientWidth * a.clientHeight)[0].src;
-    });
-
-    console.log(largest_image)
+    } catch (error) {
+        console.log(error)
+        return null
+    }
 
 }
 
-for (const url of urls)
-    scraper(url);
 
-
-
-// get all images on the page,
-//how to get image size
-//find the largest image
-//
 
 
 //organisation
