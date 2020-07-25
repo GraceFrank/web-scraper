ca800af99acb5b3771fba95bbf4fe487032e9fa5
Grace Frank <grace.frank@centricityinsights.com>
2020-07-25 13:58:59 +0100

    added url validator

--- page-details-controller.js
+++ page-details-controller.js
@@ -1,37 +1,35 @@
-const scraper = require('./scrapper')
-const { redisClient } = require('./redis-connection')
+const scraper = require("./scrapper");
+const { redisClient } = require("./redis-connection");
 
-//todo! add validation middle ware
-//add url parser middleware
+//
 
 module.exports = {
-    async get(request, response) {
-        const url = request.query.url
+  async get(request, response) {
+    const url = request.query.url;
 
-        let pageDetails
-        //check redis for pageDetails and send if present
-        try {
-            pageDetails = await redisClient.get(url)
-        } catch (err) {
-            console.log(err)
-        }
-
-        if (pageDetails)
-            return response.status(200)
-                .send({ error: false, data: JSON.parse(pageDetails) })
-
-        //scrap url
-        pageDetails = await scraper(url)
-
-        if (!pageDetails)
-            return response.status(400)
-                .send({ error: true, message: "could not get data for url" })
-
-        //store page details in redis
-        redisClient.set(url, JSON.stringify(pageDetails), (err, reply) => {
-            console.log(err)
-        })
-        return response.status(200).send({ error: false, data: pageDetails })
-
+    let pageDetails;
+    //check redis for pageDetails and send if present
+    try {
+      pageDetails = await redisClient.get(url);
+    } catch (err) {
+      console.log(err);
     }
-}
+
+    if (pageDetails)
+      return response
+        .status(200)
+        .send({ error: false, data: JSON.parse(pageDetails) });
+
+    //scrap url
+    pageDetails = await scraper(url);
+
+    if (!pageDetails)
+      return response.status(400).send({ error: true, message: "invalid url" });
+
+    //store page details in redis
+    redisClient.set(url, JSON.stringify(pageDetails), (err, reply) => {
+      console.log(err);
+    });
+    return response.status(200).send({ error: false, data: pageDetails });
+  },
+};
--- scrapper.js
+++ scrapper.js
@@ -1,36 +1,36 @@
-const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
-const scraper = async url => {
-    try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(url);
-        const html = await page.content()
-        const $ = cheerio.load(html);
+const scraper = async (url) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const html = await page.content();
+    const $ = cheerio.load(html);
 
-        // get page title from the html head
-        const pageTitle = $('title', 'head').text()
+    // get page title from the html head
+    const pageTitle = $("title", "head").text();
 
-        //get page Description from metatag
-        const pageDescription = $("meta[name|='description'], meta[property|='og\\:description']")
-            .attr('content')
+    //get page Description from metatag
+    const pageDescription = $(
+      "meta[name|='description'], meta[property|='og\\:description']"
+    ).attr("content");
 
-        //get largest image on  page
-        const largestImage = await page.evaluate(() => {
-            return [...document.getElementsByTagName('img')].sort((a, b) => b.clientWidth * b.clientHeight - a.clientWidth * a.clientHeight)[0].src;
-        });
+    //get largest image on  page
+    const largestImage = await page.evaluate(() => {
+      return [...document.getElementsByTagName("img")].sort(
+        (a, b) =>
+          b.clientWidth * b.clientHeight - a.clientWidth * a.clientHeight
+      )[0].src;
+    });
 
-        //todo: return any details found.
-        return { pageTitle, pageDescription, largestImage }
-
-    } catch (error) {
-        console.log(error)
-        return null
-    }
-
-}
-
+    return { pageTitle, pageDescription, largestImage };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
 module.exports = scraper;
 
@@ -42,8 +42,8 @@ module.exports = scraper;
 //learning
 //people and culture
 //people who have alighed with the company
-//appreciated for what am doing 
+//appreciated for what am doing
 //code review
 //being able to work with other
 
-//when i first joined  
+//when i first joined
