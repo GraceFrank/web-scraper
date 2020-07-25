c9c9c146b2db34dc7d36e41ddf3483fb64c135cc
Grace Frank <grace.frank@centricityinsights.com>
2020-07-25 01:39:29 +0100

    added caching of page details to get controller

--- page-details-controller.js
+++ page-details-controller.js
@@ -1,4 +1,5 @@
 const scraper = require('./scrapper')
+const { redisClient } = require('./redis-connection')
 
 //todo! add validation middle ware
 //add url parser middleware
@@ -7,10 +8,31 @@ const scraper = require('./scrapper')
 module.exports = {
     async get(request, response) {
         const url = request.query.url
-        const pageDetails = await scraper(url)
-        if (!pageDetails)
-            return response.status(400).send({ error: true, message: "could not get data for url" })
 
+        let pageDetails
+        //check redis for pageDetails and send if present
+        try {
+            pageDetails = await redisClient.get(url)
+        } catch (err) {
+            console.log(err)
+        }
+
+        if (pageDetails)
+            return response.status(200)
+                .send({ error: false, data: JSON.parse(pageDetails) })
+
+        //scrap url
+        pageDetails = await scraper(url)
+
+        if (!pageDetails)
+            return response.status(400)
+                .send({ error: true, message: "could not get data for url" })
+
+        //store page details in redis
+        redisClient.set(url, JSON.stringify(pageDetails), (err, reply) => {
+            console.log(err)
+        })
         return response.status(200).send({ error: false, data: pageDetails })
+
     }
 }
